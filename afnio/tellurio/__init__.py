cec3b7aaf98eef048b6affd1ed5b8943da2f2554
Piergiacomo De Marchi <piergiacomo.demarchi@gmail.com>
2025-05-10 12:18:05 +0200

    Improved login() function documentation

--- afnio/tellurio/__init__.py
+++ afnio/tellurio/__init__.py
@@ -2,17 +2,25 @@ from .client import get_default_client
 from .run import init
 
 
-def login(api_key=None, relogin=False):
+def login(api_key: str = None, relogin=False):
     """
-    Logs in the user using the default TellurioClient instance.
+    Logs in the user using an API key and verifies its validity.
+
+    This method allows the user to provide an API key or retrieve a stored API key
+    from the system. It verifies the API key by calling the backend and securely
+    stores it using the `keyring` library if valid.
 
     Args:
-        api_key (str, optional): The user's API key. If not provided, it will be
-            read from the local system.
-        relogin (bool): If True, forces a re-login and requests a new API key.
+        api_key (str, optional): The user's API key. If not provided, the method
+            attempts to retrieve a stored API key from the local system.
+        relogin (bool): If True, forces a re-login and requires the user to provide
+            a new API key.
 
     Returns:
         str: A confirmation message if the API key is valid.
+
+    Raises:
+        ValueError: If the API key is invalid or not provided during re-login.
     """
     client = get_default_client()  # Use the global accessor for _default_client
     return client.login(api_key=api_key, relogin=relogin)
