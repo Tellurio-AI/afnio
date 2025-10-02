490b1535193fe3ea903f0a8bc3f916b2fb29a4d4
Piergiacomo De Marchi <piergiacomo.demarchi@gmail.com>
2025-10-02 12:00:30 +0200

    Moved model() fixture into tests/tellurio/conftest.py.

--- tests/tellurio/test_function_sync.py
+++ tests/tellurio/test_function_sync.py
@@ -8,9 +8,7 @@ from afnio._variable import Variable
 from afnio.autodiff.basic_ops import Add, Split
 from afnio.autodiff.evaluator import DeterministicEvaluator
 from afnio.autodiff.grad_mode import no_grad
-from afnio.models.openai import AsyncOpenAI
 from afnio.tellurio import login
-from afnio.tellurio._model_registry import MODEL_REGISTRY
 from afnio.tellurio.run import init
 
 
@@ -36,22 +34,6 @@ def variables():
     x = Variable(data="abc", role="first input", requires_grad=True)
     y = Variable(data="def", role="second input", requires_grad=False)
     return x, y
-
-
-@pytest.fixture
-def model(monkeypatch):
-    """
-    Fixture to create an LM model instance.
-    """
-    # Forcing consent to sharing API keys
-    monkeypatch.setenv("ALLOW_API_KEY_SHARING", "true")
-
-    # Create OpenAI model client
-    api_key = os.getenv("OPENAI_API_KEY", "sk-test-1234567890abcdef")
-    model = AsyncOpenAI(api_key=api_key)
-    # Ensure model is registered
-    assert model.model_id in MODEL_REGISTRY
-    return model
 
 
 class TestFunctionSync:
--- tests/tellurio/test_model_sync.py
+++ tests/tellurio/test_model_sync.py
@@ -31,22 +31,6 @@ def login_and_ensure_default_run():
     return run
 
 
-@pytest.fixture
-def model(monkeypatch):
-    """
-    Fixture to create an LM model instance.
-    """
-    # Forcing consent to sharing API keys
-    monkeypatch.setenv("ALLOW_API_KEY_SHARING", "true")
-
-    # Create OpenAI model client
-    api_key = os.getenv("OPENAI_API_KEY", "sk-test-1234567890abcdef")
-    model = AsyncOpenAI(api_key=api_key)
-    # Ensure model is registered
-    assert model.model_id in MODEL_REGISTRY
-    return model
-
-
 class TestClientToServerModelSync:
     def test_create_model(self, model):
         """
